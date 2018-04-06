7f9745233bbc7249634244ba19722a7e2ada1331
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-04-06 18:54:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ bot.on('message', (message) => {
         message.reply('Demonetised!');   
     }
 });
+
+bot.on('message', (message) => {
+    if(message.content == 'Susan what are you doing right now?'){
+        message.reply('Hi, I am currently on Twitch removing all of Ninjas paid subscribers and removing ads from all of his streams, hopefully soon we will hear gun shots at Twitch HQ.');   
+    }
+});
 //
 bot.on('ready', () => { bot.user.setGame('Demonetising Ninja', 'https://www.twitch.tv/maybenima') })
 
