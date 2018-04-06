c9140acc539122928e9461301e198d68f8779063
nimahax <32222862+nimahax@users.noreply.github.com>
2018-04-06 21:35:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -76,6 +76,26 @@ bot.on('message', (message) => {
     }
 });
 */
+bot.on('message', (message) => {
+    if(message.content == '++demonetiseme'){
+        message.channel.send('What tag would you like to see? This will await will be cancelled in 30 seconds. It will finish when you provide a message that goes through the filter the first time.')
+.then(() => {
+  message.channel.awaitMessages(response => response.content === 'test', {
+    max: 1,
+    time: 30000,
+    errors: ['time'],
+  })
+  .then((collected) => {
+      message.channel.send(`The collected message was: ${collected.first().content}`);
+    })
+    .catch(() => {
+      message.channel.send('There was no collected message that passed the filter within the time limit!');
+    });
+});   
+    }
+});
+
+
 
 
 bot.on('ready', () => { bot.user.setGame('Demonetising Ninja', 'https://www.twitch.tv/maybenima') })
