2c06b6a5b2f754441c275dd7ce174f5039c45881
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-04-11 06:27:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ bot.on('message', (message) => {
    
 bot.on('message', (message) => {
     if(message.content == '++count'){
-       var text = "Susan is currently in "bot.guilds.size + "servers.";
+       var text = "Susan is currently in " + bot.guilds.size + "servers.";
         message.channel.send({embed: {
         color: 15844367,
         description: text
