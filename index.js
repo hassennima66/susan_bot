bec350c221a80b6372233bd75494a11affc613d6
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-04-12 16:17:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -594,7 +594,7 @@ bot.on('message', (message) => {
       },
       {
         "name": "**Others:**",
-        "value": "Hi/hi/Hello/hello, susan what are you doing right now?, ++meme, ++invite, +count, +info, ++help, ++tyrone "
+        "value": "Hi/hi/Hello/hello, susan what are you doing right now?, ++meme, ++invite, ++count, ++info, ++help, ++tyrone "
       },
       {
         "name": "**Youtube Links**",
