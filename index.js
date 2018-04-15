be05a11be3a14af0f3a0d56e44588046584584ef
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-04-15 04:28:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -549,35 +549,38 @@ bot.on('message', (message) => {
         description: "Would you like to be demonetised?"
 }})
 .then(() => {
-    message.channel.awaitMessages(response => response.content.toLowerCase() == 'yes' || response.content == 'no' || response.content == 'maybe', {
-        max: 1,
-        time: 30000,
-        errors: ['time'],
-        })
-            .then((collected) => {
-    if (collected.first().content == 'yes') {
-         message.channel.send({embed: {
-		color: 15844367,
-		description: "You have been demonetised, congratulations! <:demonetized:406094437236867072> :joy: ";
-            }
-    else if(collected.first().content == 'no') {
-         message.channel.send({embed: {
-		color: 15844367,
-		description: "No is not acceptable as I am Susan so you have been demonetised anyway, congratulations! <:demonetized:406094437236867072> :joy: ";
-            }
-    else if(collected.first().content == 'maybe') {
-         message.channel.send({embed: {
-		color: 15844367,
-		description: "You have to choose Yes or No. Maybe is not an answer. ";
-            }
-        })
+  message.channel.awaitMessages(response => response.content.toLowerCase() == 'yes' || response.content == 'no' || response.content == 'maybe', {
+    max: 1,
+    time: 30000,
+    errors: ['time'],
+  })
+  .then((collected) => {
+     // message.channel.send(`You have been demonetised as your response message was: ${collected.first().content}`);
+      if (collected.first().content == 'yes') {
+		  message.channel.send({embed: {
+        color: 15844367,
+        description: "You have been demonetised, congratulations! <:demonetized:406094437236867072> :joy: "
+	  }
+	  else if(collected.first().content == 'no') {
+		  message.channel.send({embed: {
+        color: 15844367,
+        description: "No is not acceptable as I am Susan so you have been demonetised anyway, congratulations! <:demonetized:406094437236867072> :joy: "
+	  }
+	  else if(collected.first().content == 'maybe') {
+		  message.channel.send({embed: {
+        color: 15844367,
+        description: "You have to choose Yes or No. Maybe is not an answer. <:whatamigonnado:409287809325727744> "	  
+}})      
+    })
     .catch(() => {
          message.channel.send({embed: {
-	    color: 15844367,
-		description: "There was no response in 30 seconds, you have been demonetised anyway mate!!! <:demonetized:406094437236867072> :point_right: :joy: :point_right:<:demonetized:406094437236867072> ";
-        });
-    });         
-}
+        color: 15844367,
+        description: "There was no response in 30 seconds, you have been demonetised anyway mate!!! <:demonetized:406094437236867072> :point_right: :joy: :point_right:<:demonetized:406094437236867072> "
+}})      
+   //   message.channel.send('There was no response in 30 seconds, you have been demonetised anyway mate!!!  :point_right: :joy: :point_right: ');
+    });
+});   
+    }
 });
 
 /*
