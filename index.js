c03fdaa8fd42686721f5907db72fc47335aadb0f
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-04-15 07:55:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -570,6 +570,9 @@ bot.on('message', (message) => {
 	  }
 	  else if(collected.first().content == 'maybe') {
 		  message.channel.send({embed: {
+    max: 1,
+    time: 30000,
+    errors: ['time'],
         color: 15844367,
         description: "You have to choose Yes or No. Maybe is not an answer. <:whatamigonnado:409287809325727744> "	  
 }}) 
