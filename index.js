4b1b873523715afd01c3a3c4b51dda1204aca8a5
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-05-01 04:46:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -459,6 +459,39 @@ var taggedmember1 = message.mentions.members.first();
 
 
 
+const { Command } = require('++choice');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['choose', 'decide'],
+
+			description: 'Makes a decision for your given life choices.',
+			usage: '<choices:str> [...]',
+			usageDelim: ', '
+		});
+	}
+
+	async run(msg, [...choices]) {
+		return msg.reply(choices.length === 1 ?
+			'You only gave me one choice, dummy.' :
+			`If I were you, I'd choose ${choices[Math.floor(Math.random() * choices.length)]}`);
+}});
+   
+   
+} 
+			 
+		
+
+
+}
+}
+});
+
+
+
+
 
 /////////END HERE
 
