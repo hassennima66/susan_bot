82f10f0f37569f70c141eaf7b43f3269fb53fae0
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-05-01 04:52:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -469,38 +469,6 @@ var taggedmember1 = message.mentions.members.first();
 
 
 
-bot.on('message', (message) => {
-    if(message.content == '++choice'){
-
-	constructor(...args) {
-		super(...args, {
-			aliases: ['choose', 'decide'],
-
-			description: 'Makes a decision for your given life choices.',
-			usage: '<choices:str> [...]',
-			usageDelim: ', '
-		});
-	}
-
-	async run(msg, [...choices]) {
-		return msg.reply(choices.length === 1 ?
-			'You only gave me one choice, dummy.' :
-			`If I were you, I'd choose ${choices[Math.floor(Math.random() * choices.length)]}`);
-}});
-   
-   
-} 
-			 
-		
-
-
-}
-}
-});
-
-
-
-
 
 bot.on('message', (message) => {
     if (message.content.indexOf("++gay") !=-1) {
