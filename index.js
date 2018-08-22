f03b10192f3c64a3e8b3f51bdc75844d1a89ebe4
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-22 21:14:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const config = require('./config.json');
 const client = new Discord.Client({ fetchAllMembers: true, sync: true });
 
 
