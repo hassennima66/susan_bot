18097cd163a60770ee2384c12a88d8334d32063a
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-22 04:06:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1041,7 +1041,33 @@ bot.on('message', function(message) {
             .catch(console.error); // add error handling here
         }, 1 * 1000); 
     }
-});    
+});   
+
+client.on('message', (message) => {
+  const args = message.content.split(" ").slice(1);
+	
+    if (message.content.startsWith("++eval")) {
+    if(message.author.id !== "136191833196855296") return;
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+
+      message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+  }
+});
+
+function clean(text) {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
 
 
 
